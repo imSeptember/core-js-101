b4a730e1b81906200bcccb87ec5cfa8bdf086a7b
Vladislav Kalenskiy <108691131+imSeptember@users.noreply.github.com>
2023-09-17 18:27:43 +0300

    Update 04-conditions-n-loops-tasks.js

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -419,35 +419,35 @@ function getMatrixProduct(/* m1, m2 */) {
  *    [    ,   ,    ]]
  *
  */
-function evaluateTicTacToePosition(position) {
-  for (let i = 0; i < 3; i += i) {
-    if (
-      position[i][0] === position[i][1]
-      && position[i][1] === position[i][2]
-    ) {
-      if (position[i][0] === 'X' || position[i][0] === 'O') {
-        return position[i][0];
-      }
-    }
-    if (
-      position[0][i] === position[1][i]
-      && position[1][i] === position[2][i]
-    ) {
-      if (position[0][i] === 'X' || position[0][i] === 'O') {
-        return position[0][i];
-      }
-    }
-  }
-  if (
-    (position[0][0] === position[1][1] && position[1][1] === position[2][2])
-    || (position[0][2] === position[1][1] && position[1][1] === position[2][0])
-  ) {
-    if (position[1][1] === 'X' || position[1][1] === 'O') {
-      return position[1][1];
-    }
-  }
-  return undefined;
-}
+// function evaluateTicTacToePosition(position) {
+//   for (let i = 0; i < 3; i += i) {
+//     if (
+//       position[i][0] === position[i][1]
+//       && position[i][1] === position[i][2]
+//     ) {
+//       if (position[i][0] === 'X' || position[i][0] === 'O') {
+//         return position[i][0];
+//       }
+//     }
+//     if (
+//       position[0][i] === position[1][i]
+//       && position[1][i] === position[2][i]
+//     ) {
+//       if (position[0][i] === 'X' || position[0][i] === 'O') {
+//         return position[0][i];
+//       }
+//     }
+//   }
+//   if (
+//     (position[0][0] === position[1][1] && position[1][1] === position[2][2])
+//     || (position[0][2] === position[1][1] && position[1][1] === position[2][0])
+//   ) {
+//     if (position[1][1] === 'X' || position[1][1] === 'O') {
+//       return position[1][1];
+//     }
+//   }
+//   return undefined;
+// }
 
 
 module.exports = {
